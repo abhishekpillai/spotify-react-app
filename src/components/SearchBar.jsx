7f119e622613353b68c5abde1433adc529e5ae27
Abhi Pillai <abhi.pillai1@gmail.com>
2016-09-25 20:48:05 -0400

    Setup search bar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(ev) {
+    this.setState({
+      searchTerm: ev.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <input onChange={this.handleInputChange} />
+    );
+  }
+}
+
+export default SearchBar;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-const foo = 'bar';
-foo.toUpperCase();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
 
